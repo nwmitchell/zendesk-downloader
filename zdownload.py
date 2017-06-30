94ea6a0694d0ea5bb46ebf241e4c72aa11e7df0c
Nick Mitchell <nmitchell@cumulusnetworks.com>
2017-06-30 12:22:00 -0400

    Fix unicode versus string if Zendesk org doesn't exist

--- zdownload.py
+++ zdownload.py
@@ -101,6 +101,8 @@ def main():
         case_info = zendesk.getCaseInfo(ticket)
 
         if not "error" in case_info:
+            if case_info['org_name'] == "None":
+                case_info['org_name'] = u"None"
             case_info['org_name'] = case_info['org_name'].replace("+","")
             case_info['org_name'] = unicodedata.normalize('NFKC', case_info['org_name']).encode('ascii', 'ignore')
             try:
